4a4b8cc4731b2ae1d2d00e5541ff318f806d7227
Aleksandr <aleksandr.evlanicov@gmail.com>
2024-04-21 17:15:28 +0300

    Добавьте файлы проекта.

---
+++ ArrayNumConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace ArrayNumConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine("Введите количество строк в массиве: ");
+            int inputLine = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов в массиве: ");
+            int inputColumn = int.Parse(Console.ReadLine());
+            Console.WriteLine("________________________________________");
+            int sumOne = 0;
+            int sumTwo = 0;
+
+            int[,] arrayNumOne = new int[inputLine, inputColumn];
+            int[,] arrayNumTwo = new int[inputLine, inputColumn];
+
+
+            for (int i = 0; i < inputLine; i++)
+            {
+                for (int j = 0; j < inputColumn; j++)
+                {
+                    arrayNumOne[i, j] = random.Next(10);
+                    Console.Write($"{arrayNumOne[i, j]} ");
+                    sumOne = sumOne + arrayNumOne[i, j];
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("----------------------");
+            Console.WriteLine($"Сумма первого массива = {sumOne}");
+            Console.WriteLine("----------------------");
+
+
+            for (int i = 0; i < inputLine; i++)
+            {
+                for (int j = 0; j < inputColumn; j++)
+                {
+                    arrayNumTwo[i, j] = random.Next(10);
+                    Console.Write($"{arrayNumTwo[i, j]} ");
+                    sumTwo = sumTwo + arrayNumTwo[i, j];
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("----------------------");
+            Console.WriteLine($"Сумма второго массива = {sumTwo}");
+            Console.WriteLine("----------------------");
+
+
+            for (int i = 0; i < inputLine; i++)
+            {
+                for (int j = 0; j < inputColumn; j++)
+                {
+                    var arraySum = arrayNumOne[i, j] + arrayNumTwo[i, j];
+                    Console.Write($"{arraySum} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("----------------------");
+            Console.WriteLine($"Сумма третьего массива = {sumTwo + sumOne}");
+            Console.WriteLine("----------------------");
+
+        }
+    }
+}
